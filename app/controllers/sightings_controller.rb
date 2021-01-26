22d4290ee9582cced80eeeceeb8378862bcf0540
Raveen K Sharma <raveen08@gmail.com>
2021-01-26 20:16:03 +0000

    Automatically backed up by Learn

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,3 +1,10 @@
 class SightingsController < ApplicationController
+  def index
+  sightings = Sighting.all
+  render json: sightings.to_json(include: [:bird, :location])
+  end
+  
+  
+  
   
 end
